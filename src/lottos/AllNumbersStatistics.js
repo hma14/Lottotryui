db51f64d1d7d0e4148b447460b302a2dcea48f0e
Henry Ma <henryma14@gmail.com>
2021-11-11 21:01:31 -0800

    layout

--- src/lottos/AllNumbersStatistics.js
+++ src/lottos/AllNumbersStatistics.js
@@ -16,7 +16,7 @@ function AllNumbersStatistics (props) {
           <Table className="small table-hover table-borderless table-info mb-4 -highlight" >
             <thead className="table-danger text-center">
                 <tr>
-                  <th className="text-light bg-primary">Draws</th>
+                  <th className="text-light bg-info">Draws</th>
                   <th className="text-light bg-info">Date</th>
                   {props.lottoData.slice(0, 1).map(row => row.numbers.map((no) => <th key={no.number} className='text-warning bg-success'>{no.number}</th>))}
                 </tr>
@@ -25,9 +25,9 @@ function AllNumbersStatistics (props) {
                  {props.lottoData.map(row =>                       
                         <tr key={row.drawNumber}>
                             <td className="text-light bg-primary">{row.drawNumber}</td>
-                            <td className="text-light bg-info">{moment(row.drawDate).format('yyyy-MM-DD')}</td>
+                            <td className="text-warning bg-success">{moment(row.drawDate).format('yyyy-MM-DD')}</td>
                             {row.numbers.map(no => 
-                            no.isHit === true ? (<td className='text-primary bg-warning fw-bold' key={no.number}>{no.number}<br />({no.distance})<br />(<span className='text-danger fst-italic'>{no.numberofDrawsWhenHit}</span>)<br />(<span className='text-dark fst-italic'>{no.totalHits}</span>)</td>) 
+                            no.isHit === true ? (<td className='text-primary bg-warning fw-bold' key={no.number}>{no.number}<br />(<span className='text-danger fst-italic'>{no.numberofDrawsWhenHit}</span>)<br />(<span className='text-dark fst-italic'>{no.totalHits}</span>)</td>) 
                                               : (no.distance > 10 ? (<td className='text-success bg-light fw-bold' key={no.number}>{no.number}<br />(<span className='text-danger fst-italic'>{no.distance}</span>)<br />(<span className='text-primary fst-italic'>{no.totalHits}</span>)</td>) 
                                               : (<td className='text-success bg-light fw-bold' key={no.number}>{no.number}<br />(<span className='text-success fst-italic'>{no.distance}</span>)<br />(<span className='text-primary fst-italic'>{no.totalHits}</span>)</td>))
                             )}   
