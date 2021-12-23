5214142d5d24b7589ddafa1c51fd5a3514f263d4
henry <henryma14@gmail.com>
2021-12-23 14:09:07 -0800

    Changed background colors for Sort by Distance

--- src/App.js
+++ src/App.js
@@ -194,7 +194,7 @@ function App() {
                     <select id="rpp" className="dropdown btn btn-success dropdown-toggle ps-4 fw-bold"  
                       value={pageSize}
                       onChange={(e) => setPageSize(e.target.value)}>
-                      {[10, 20, 30, 40, 50].map(pageSize => (
+                      {[5, 10, 20, 30, 40, 50].map(pageSize => (
                         <option key={pageSize} value={pageSize}> {pageSize}</option>
                       ))}
                     </select>
--- src/lottos/AllNumbersStatistics.js
+++ src/lottos/AllNumbersStatistics.js
@@ -1,6 +1,7 @@
 import React  from 'react'
 import {Table} from 'react-bootstrap'
 import moment   from 'moment'
+
 import '../App.css'
 
 
@@ -9,12 +10,55 @@ const AllNumbersStatistics = (props) => {
 
   const {lottoData, sortType} = props 
 
+  console.log(sortType)
+
   const types = {
     number: 'number',
     distance: 'distance',
     totalHits: 'totalHits',
   }
   const sortProperty = types[sortType]
+
+  const getBgColors = (sortType, distance) => {
+    if (sortType === types.distance)
+    {
+      switch(distance)
+      {
+        case 1: return 'distanceBgColor1 text-success fw-bold'
+        case 2: return 'distanceBgColor2 text-success fw-bold'
+        case 3: return 'distanceBgColor3 text-success fw-bold'
+        case 4: return 'distanceBgColor4 text-success fw-bold'
+        case 5: return 'distanceBgColor5 text-success fw-bold'
+        case 6: return 'distanceBgColor6 text-success fw-bold'
+        case 7: return 'distanceBgColor7 text-success fw-bold'
+        case 8: return 'distanceBgColor8 text-success fw-bold'
+        case 9: return 'distanceBgColor9 text-success fw-bold'
+        case 10: return 'distanceBgColor10 text-success fw-bold'
+        case 11: return 'distanceBgColor11 text-success fw-bold'
+        case 12: return 'distanceBgColor12 text-success fw-bold'
+        case 13: return 'distanceBgColor13 text-success fw-bold'
+        case 14: return 'distanceBgColor14 text-success fw-bold'
+        case 15: return 'distanceBgColor15 text-success fw-bold'
+        case 16: return 'distanceBgColor16 text-success fw-bold'
+        case 17: return 'distanceBgColor17 text-success fw-bold'
+        case 18: return 'distanceBgColor18 text-success fw-bold'
+        case 19: return 'distanceBgColor19 text-success fw-bold'
+        case 20: return 'distanceBgColor20 text-success fw-bold'
+        case 21: return 'distanceBgColor21 text-success fw-bold'
+        case 22: return 'distanceBgColor22 text-success fw-bold'
+        case 23: return 'distanceBgColor23 text-success fw-bold'
+        case 24: return 'distanceBgColor24 text-success fw-bold'
+        case 25: return 'distanceBgColor25 text-success fw-bold'
+      }
+    }
+    else{
+      return  'text-success bg-light fw-bold'
+    }
+  }
+
+  
+  
+ 
 
   return (
 
@@ -45,8 +89,8 @@ const AllNumbersStatistics = (props) => {
                                                         (<span className='text-danger fst-italic'>{no.numberofDrawsWhenHit}</span>)<br />
                                                         (<span className='text-secondary fst-italic'>{no.totalHits}</span>)</td>) )
                             
-                                              : (no.distance > 10 ? (<td className='text-success bg-light fw-bold' key={no.value}>{no.value}<br />(<span className='my-color-1 fst-italic'>{no.distance}</span>)<br />(<span className='text-primary fst-italic'>{no.totalHits}</span>)</td>) 
-                                              : (<td className='text-success bg-light fw-bold' key={no.value}>{no.value}<br />(<span className='text-success fst-italic'>{no.distance}</span>)<br />(<span className='text-primary fst-italic'>{no.totalHits}</span>)</td>))
+                                              : (no.distance > 10 ? (<td className={getBgColors(sortType, no.distance)}  key={no.value}>{no.value}<br />(<span className='my-color-1 fst-italic'>{no.distance}</span>)<br />(<span className='text-primary fst-italic'>{no.totalHits}</span>)</td>) 
+                                              : (<td className={getBgColors(sortType, no.distance)} key={no.value}>{no.value}<br />(<span className='text-success fst-italic'>{no.distance}</span>)<br />(<span className='text-primary fst-italic'>{no.totalHits}</span>)</td>))
                             )
                           }
                         </tr>
